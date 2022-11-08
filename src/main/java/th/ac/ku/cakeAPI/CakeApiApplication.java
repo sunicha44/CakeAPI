1bc1fb05679c6cd498ebfa2e5926d42e8117f8a3
Sunicha <sunicha.f@ku.th>
2022-11-08 22:21:37 +0700

    cakeAPI project structure

---
+++ src/main/java/th/ac/ku/cakeAPI/CakeApiApplication.java
@@ -0,0 +1,13 @@
+package th.ac.ku.cakeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeApiApplication.class, args);
+	}
+
+}
